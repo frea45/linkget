c82f8397b3a95ba674f674703e80765bd9ab2350
frea45 <110092585+frea45@users.noreply.github.com>
2022-09-03 12:07:36 +0430

    Update __init__.py

--- config/__init__.py
+++ config/__init__.py
@@ -1,8 +1,8 @@
 import os
 
 class Config:
-    API_ID = int( os.getenv("api_id","") )
-    API_HASH = os.getenv("api_hash","")
+    API_ID = int( os.getenv("api_id","3335796") )
+    API_HASH = os.getenv("api_hash","138b992a0e672e8346d8439c3f42ea78")
     CHANNEL = int( os.getenv("channel_files_chat_id","-1001787336960") )
     CHANNEL_USERNAME = os.getenv("channel_username","ir_botz")
     TOKEN = os.getenv("token","1943275919:AAFwSScrlR_i-AzdNTOiw8EEctbU_exOaGg")
