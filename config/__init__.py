b2624db9f05eb39e7b7d537979983f9453ce0f80
frea45 <110092585+frea45@users.noreply.github.com>
2022-09-03 11:59:46 +0430

    Update __init__.py

--- config/__init__.py
+++ config/__init__.py
@@ -1,9 +1,9 @@
 import os
 
 class Config:
-    API_ID = int( os.getenv("api_id","1234") )
-    API_HASH = os.getenv("api_hash","21ab7cb0a453b5e60016dc7bbeb701cb")
-    CHANNEL = int( os.getenv("channel_files_chat_id","-1001249461809") )
-    CHANNEL_USERNAME = os.getenv("channel_username","UserLandapp")
-    TOKEN = os.getenv("token","xxxxx")
-    DOMAIN  = os.getenv("domain","https://newdlstar.herokuapp.com")
+    API_ID = int( os.getenv("api_id","") )
+    API_HASH = os.getenv("api_hash","")
+    CHANNEL = int( os.getenv("channel_files_chat_id","-1001787336960") )
+    CHANNEL_USERNAME = os.getenv("channel_username","ir_botz")
+    TOKEN = os.getenv("token","1943275919:AAFwSScrlR_i-AzdNTOiw8EEctbU_exOaGg")
+    DOMAIN  = os.getenv("domain","https://dlchin.herokuapp.com")
